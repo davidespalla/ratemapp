6b67130bcf96638c4b3f425a83160fe0be8c75d6
davidespalla <davide.spalla@gmail.com>
2022-08-02 11:49:57 +0200

    added file upload

--- app.R
+++ app.R
@@ -1,5 +1,6 @@
 library(shiny)
 library(ggplot2)
+options(shiny.maxRequestSize = 100*1024^2)
 
 ui <- fluidPage(
   
@@ -10,13 +11,28 @@ ui <- fluidPage(
   sidebarLayout(
     #populate sidebar panel
     sidebarPanel(
+      fileInput("file_spikes", "Choose spikes file",
+                multiple = FALSE,
+                accept = c("text/csv",
+                           "text/comma-separated-values,text/plain",
+                           ".csv")),
+      
+      fileInput("file_bh", "Choose behaviour file",
+                multiple = FALSE,
+                accept = c("text/csv",
+                           "text/comma-separated-values,text/plain",
+                           ".csv")),
+      
       selectInput('selected_cell','Select a cell',choices = c(1:10))
     ), # end side panel
     
     #populate main panel
     mainPanel(
       verbatimTextOutput('text'),
+      tableOutput('df_table'),
+      plotOutput('position_plot',width = '600px'),
       plotOutput('tuning_curve',width = '600px')
+
     ) # end main panel
     
   ) # end sidebar layout
@@ -24,9 +40,16 @@ ui <- fluidPage(
 ) # end ui
 
 server <- function(input,output,session){
-  # read input 
-  selected_cell <- reactive(input$selected_cell)
 
+  # read input 
+  df = reactive({
+    req(input$file_bh)
+    
+    df <- read.csv(input$file_bh$datapath)
+    
+    return(head(df,1000))
+  })
+  selected_cell = reactive(input$selected_cell)
   # generate data
   x <- seq(0,100,0.5)
   s <- 10
@@ -39,8 +62,15 @@ server <- function(input,output,session){
     tuning_matrix[i,] <- dnorm(x,m[i],s)
   }
   
+  
+  t <- reactive(df()[,1])
+  pos <- reactive(df()[,2])
+  
+  
   # build outputs
   output$text <- renderPrint(selected_cell()) #selcted_cell(input$selected_cell)
+  output$df_table <-renderTable(head(df()))
+  output$position_plot <- renderPlot(plot(t(),pos()),res=96)
   output$tuning_curve <- renderPlot(plot(x,tuning_matrix[as.integer(selected_cell()),]),res=96)
 }
 
