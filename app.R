68620db57508b9c3062e198dc80589aa26a001d8
davidespalla <davide.spalla@gmail.com>
2022-08-10 18:19:59 +0200

    added tuning curve function

--- app.R
+++ app.R
@@ -1,5 +1,8 @@
+rm(list = ls())
+
 library(shiny)
 library(ggplot2)
+source("tools/functions.R")
 options(shiny.maxRequestSize = 100*1024^2)
 
 ui <- fluidPage(
@@ -29,8 +32,6 @@ ui <- fluidPage(
     #populate main panel
     mainPanel(
       verbatimTextOutput('text'),
-      tableOutput('df_table'),
-      plotOutput('position_plot',width = '600px'),
       plotOutput('tuning_curve',width = '600px')
 
     ) # end main panel
@@ -41,39 +42,56 @@ ui <- fluidPage(
 
 server <- function(input,output,session){
 
-  # read input 
-  df = reactive({
+  # read behaviour file
+  bh_df = reactive({
     req(input$file_bh)
     
-    df <- read.csv(input$file_bh$datapath)
+    df <- read.csv(input$file_bh$datapath,header=FALSE)
+    colnames(df) <- c('t','x')
     
-    return(head(df,1000))
+    
+    return(df)
   })
+  
+  # read spike files
+  spike_df = reactive({
+    req(input$file_spikes)
+    
+    df <- read.csv(input$file_spikes$datapath,header=FALSE)
+    
+    return(df)
+  })
+  
+  # read selected cell
   selected_cell = reactive(input$selected_cell)
-  # generate data
-  x <- seq(0,100,0.5)
-  s <- 10
-  n_cells <- 10
-  m <- seq(0,100,n_cells)
-
-  tuning_matrix <- matrix(, nrow = n_cells, ncol = length(x))
-
-  for(i in seq(1,n_cells)){
-    tuning_matrix[i,] <- dnorm(x,m[i],s)
-  }
   
   
-  t <- reactive(df()[,1])
-  pos <- reactive(df()[,2])
   
+  
+  tuning_curve <- reactive({
+    req(input$file_bh)
+    req(input$file_spikes)
+    bh_df_ <- bh_df()
+    spikes_df_ <- spike_df()
+    selected_cell_ <- as.integer(selected_cell())
+    
+    tuning_curve_ <- build_tuning_curve(bh_df_,spikes_df_,
+                                        cell_n=selected_cell_,bin_size=0.1)
+    return(tuning_curve_)
+  })
   
   # build outputs
-  output$text <- renderPrint(selected_cell()) #selcted_cell(input$selected_cell)
-  output$df_table <-renderTable(head(df()))
-  output$position_plot <- renderPlot(plot(t(),pos()),res=96)
-  output$tuning_curve <- renderPlot(plot(x,tuning_matrix[as.integer(selected_cell()),]),res=96)
+  output$text <- renderPrint(tuning_curve()) #selcted_cell(input$selected_cell)
+  #output$position_plot <- renderPlot(plot(t(),pos()),res=96)
+  #output$tuning_curve <- renderPlot(plot(x,tuning_matrix[as.integer(selected_cell()),]),res=96)
+  output$tuning_curve <- renderPlot(plot(tuning_curve()$x,tuning_curve()$y),res=96)
+  
 }
 
 
 
 shinyApp(ui,server)
+
+#spikes <- read.csv(file = 'data/spikes.csv', header = FALSE)
+#behaviour <- read.csv(file = 'data/behaviour.csv', header = FALSE)
+#colnames(behaviour) <- c('t','x')
---
+++ tools/functions.R
@@ -0,0 +1,29 @@
+build_tuning_curve <- function(behaviour_df,spikes_df,cell_n=1,bin_size=0.1) {
+  n_bins <- as.integer(1./bin_size)
+  filt_bh <-subset(behaviour_df,behaviour_df$x>0 & behaviour_df$x<1) # change with given range
+  
+  #occupancy histogram
+  occupancy_h <- hist(filt_bh$x,breaks=n_bins,plot=FALSE);
+
+  
+  # spike histogram
+  spike_pos<- approx(filt_bh$t,filt_bh$x,spikes_df[cell_n,])$y
+  spike_h <- hist(spike_pos,breaks=n_bins,plot=FALSE);
+
+  occupancy <- list('x'=occupancy_h$mids,'y'=occupancy_h$counts)
+  tuning_curve <- list('x'=spike_h$mids,'y'= spike_h$counts/occupancy_h$counts)
+  
+  return(tuning_curve) #(list('occupancy'=occupancy,'tuning_curve'=tuning_curve))
+  
+  # add smoothing
+  
+}
+
+#spikes <- read.csv(file = 'data/spikes.csv', header = FALSE)
+#behaviour <- read.csv(file = 'data/behaviour.csv', header = FALSE)
+#colnames(behaviour) <- c('t','x')
+
+#h <- build_tuning_curve(behaviour,spikes,10,bin_size = 0.05)
+#plot(h$x,h$y)
+
+#plot(h$tuning_curve$x,h$tuning_curve$y)
